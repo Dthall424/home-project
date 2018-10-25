3e1bd597d6acc3cc98ea9071ebfdfc320211e674
Dthall424 <hall.dylan46@yahoo.com>
2018-10-24 22:44:10 -0400

    added the base JSX for the home page, waiting to be snipped into components directly.

--- home-project/src/App.js
+++ home-project/src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
+import './CLPro.css';
 
 class App extends Component {
   render() {
     return (
-      <header className="sticky">
-        <ul className="main-nav">
-          <li><a href = "..\HTML\CLPro.html">Home</a></li>
-          <li><a href = "..\HTML\CLabout.html">About</a></li>
-          <li><a href = "..\HTML\CLcontact.html">Contact</a></li>
-        </ul>
-      </header>
+
+      <body>
+        <header className="sticky">
+          <ul className="main-nav">
+            <li><a href = "..\HTML\CLPro.html">Home</a></li>
+            <li><a href = "..\HTML\CLabout.html">About</a></li>
+            <li><a href = "..\HTML\CLcontact.html">Contact</a></li>
+          </ul>
+        </header>
+        <div className="wrapper">
+          <h1 className="mainhead">
+            Dylan Hall<br></br>
+            Junior Software Developer
+          </h1>
+        <div>
+          <p className="flex">
+            In laoreet purus vel orci porttitor dictum. Suspendisse eget luctus eros. Aenean a nibh dictum arcu maximus dapibus et id risus. Aenean gravida nunc quis quam vehicula, at iaculis sapien dictum. Fusce lectus nisl, fermentum sed eleifend ac, volutpat vel elit. In hac habitasse platea dictumst. Phasellus nec finibus justo. Nullam vehicula metus dui, ac rhoncus purus porttitor feugiat. Suspendisse commodo interdum neque, vel bibendum augue viverra sed. Maecenas posuere nulla non augue facilisis laoreet. Praesent et tellus aliquam, efficitur enim a, finibus arcu. Cras nisi purus, mollis at volutpat finibus, cursus a nunc.
+          </p>
+          <p className="item">
+            Sed molestie tellus ac lectus accumsan, quis cursus elit imperdiet. Fusce et nunc at massa interdum tincidunt at in nunc. Sed iaculis malesuada tellus, et hendrerit risus cursus dictum. Sed in neque rhoncus, mollis sapien eu, egestas metus. Fusce eu ullamcorper mauris. Vivamus rutrum, elit et vulputate lobortis, ligula enim mollis urna, et facilisis diam enim non purus. Vivamus at efficitur purus. Aliquam id elementum quam. Nullam vehicula metus dui, ac rhoncus purus porttitor feugiat. Suspendisse commodo interdum neque, vel bibendum augue viverra sed. Maecenas posuere nulla non augue facilisis laoreet. Praesent et tellus aliquam, efficitur enim a, finibus arcu. Cras nisi purus, mollis at volutpat finibus, cursus a nunc. Donec tincidunt mauris felis, id tempor risus auctor vel. Quisque dapibus mauris varius enim aliquam placerat.
+        </p>
+        <p className="item">
+          dolor sit amet, consectetur adipiscing elit. Sed nec finibus neque, et aliquet turpis. Maecenas finibus aliquam tortor, in pulvinar mauris lacinia quis. Proin iaculis, est id facilisis pretium, tellus enim imperdiet eros, ut mattis ex libero quis sapien. Praesent posuere erat at ex bibendum, in accumsan dolor sollicitudin. Ut condimentum est quis vestibulum accumsan. Integer tempus lacinia risus. Mauris ac sollicitudin nunc, non blandit mauris. Donec tempor faucibus bibendum. Nunc elementum odio sed ipsum vehicula pharetra. Praesent diam quam, consectetur et dui et, mattis vestibulum ante. Vivamus tempus a magna ac tincidunt. Sed at cursus diam. Morbi suscipit ante non massa hendrerit vehicula. Sed viverra tellus non pharetra varius. Morbi eget ante consectetur augue rhoncus blandit.
+        </p>
+        <p className="item">
+          ipsum dolor sit amet, consectetur adipiscing elit. Sed nec finibus neque, et aliquet turpis. Maecenas finibus aliquam tortor, in pulvinar mauris lacinia quis. Proin iaculis, est id facilisis pretium, tellus enim imperdiet eros, ut mattis ex libero quis sapien. Praesent posuere erat at ex bibendum, in accumsan dolor sollicitudin. Ut condimentum est quis vestibulum accumsan. Integer tempus lacinia risus. Mauris ac sollicitudin nunc, non blandit mauris. Donec tempor faucibus bibendum. Nunc elementum odio sed ipsum vehicula pharetra. Praesent diam quam, consectetur et dui et, mattis vestibulum ante. Vivamus tempus a magna ac tincidunt. Sed at cursus diam. Morbi suscipit ante non massa hendrerit vehicula. Sed viverra tellus non pharetra varius. Morbi eget ante consectetur augue rhoncus blandit.
+        </p>
+      </div>
+    </div>
+  </body>
     );
   }
 }
---
+++ home-project/src/Contact.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import './CLPro.css';
+
+class Contact extends react.Component{
+  
+}
